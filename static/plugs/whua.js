58bc099fd7da4594bce95aba400b6ccbc8e6f2cb
382379437@qq.com <382379437@qq.com>
2018-08-28 09:54:56 +0800

    wangh.js增加字符转义和反转义方法，用于参数传递之前转义

--- static/plugs/whua.js
+++ static/plugs/whua.js
@@ -683,5 +683,36 @@ window.wh = {
         }
         //默认返回false 表示没有取到值
         return false;
+    },
+    //字符转义
+    html_encode: function(str){
+        var s = "";
+        if (str.length == 0) return "";
+        s = str.replace(/&/g, "&amp;");
+        s = s.replace(/</g, "&lt;");
+        s = s.replace(/>/g, "&gt;");
+        s = s.replace(/ /g, "&nbsp;");
+        s = s.replace(/\'/g, "&#39;");
+        s = s.replace(/\"/g, "&quot;");
+        s = s.replace(/\n/g, "<br/>");
+        s = s.replace(/=/g, "&egt;");
+        //add more
+
+        return s;
+    },
+    //字符反转义
+    html_decode: function (str){
+        var s = "";
+        if (str.length == 0) return "";
+        s = str.replace(/&amp;/g, "&");
+        s = s.replace(/&lt;/g, "<");
+        s = s.replace(/&gt;/g, ">");
+        s = s.replace(/&nbsp;/g, " ");
+        s = s.replace(/&#39;/g, "\'");
+        s = s.replace(/&quot;/g, "\"");
+        s = s.replace(/<br\/>/g, "\n");
+        s = s.replace(/&egt;/g, "=");
+        //add more
+        return s;
     }
 };
